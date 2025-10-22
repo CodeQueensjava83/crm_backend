56010fc401c98dc7aad71162300c3d6a1bad3f30
carina <carinabentlin@gmail.com>
2025-10-22 20:04:01 -0300

    Atualização nos atributos de Oportunidades

--- src/main/java/com/generation/crm/model/Oportunidade.java
+++ src/main/java/com/generation/crm/model/Oportunidade.java
@@ -1,43 +1,29 @@
 package com.generation.crm.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "tb_oportunidades")
 public class Oportunidade {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // id autoincrementado
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "A descrição é obrigatória!")
+    @NotBlank(message = "A descrição é obrigatória")
     private String descricao;
 
-    @NotNull(message = "O valor é obrigatório!")
+    @NotNull(message = "O valor não pode ser nulo")
     private BigDecimal valor;
 
-    @NotNull(message = "A data de criação é obrigatória!")
-    private LocalDate dataCriacao;
+    @NotBlank(message = "O status é obrigatório")
+    private String status;
+    // Exemplo: aberta, fechada e perdida
 
-    // Relacionamento ManyToOne com Cliente
-    @ManyToOne
-    @JoinColumn(name = "cliente_id")
-    @JsonIgnoreProperties("oportunidades")
-    private Cliente cliente;
-
-    // Relacionamento ManyToOne com Usuario
-    @ManyToOne
-    @JoinColumn(name = "usuario_id")
-    @JsonIgnoreProperties("oportunidades")
-    private Usuario usuario;
-
-    // Getters e setters
+    // Getters e Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -47,12 +33,6 @@ public class Oportunidade {
     public BigDecimal getValor() { return valor; }
     public void setValor(BigDecimal valor) { this.valor = valor; }
 
-    public LocalDate getDataCriacao() { return dataCriacao; }
-    public void setDataCriacao(LocalDate dataCriacao) { this.dataCriacao = dataCriacao; }
-
-    public Cliente getCliente() { return cliente; }
-    public void setCliente(Cliente cliente) { this.cliente = cliente; }
-
-    public Usuario getUsuario() { return usuario; }
-    public void setUsuario(Usuario usuario) { this.usuario = usuario; }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
