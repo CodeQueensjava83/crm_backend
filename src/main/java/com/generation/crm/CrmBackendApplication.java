30a6712e6380781955302c481c3409d8d194161f
Milena <milenasvitras@gmail.com>
2025-12-03 07:57:47 -0300

    sparkles: Atualizações de variáveis de ambiente

--- src/main/java/com/generation/crm/CrmBackendApplication.java
+++ src/main/java/com/generation/crm/CrmBackendApplication.java
@@ -3,10 +3,23 @@ package com.generation.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CrmBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CrmBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		
+
+		try {
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		} catch (Exception e) {
+			System.out.println("Running without .env file - using environment variables");
+}
+		
+		SpringApplication.run(CrmBackendApplication.class, args);
+	}
+
 }
